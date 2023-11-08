11c42d6ae811a3f8b2bb44b728b9331900a0697f
Vijji455 <149236290+Vijji455@users.noreply.github.com>
2023-11-08 16:04:42 +0000

    conditional statements

---
+++ Day-07/condstmnt.py
@@ -0,0 +1,10 @@
+import sys
+type = sys.argv[1]
+if type == "python":
+    print("pyhton1")    
+elif type == "java":
+    print("java1")
+elif type == "c++":
+    print("c1")
+else:
+    print("wrong input")    
